aa2d72b2170cb3d45247e4bbbdcb33c2f20defa8
nathan <nathanlogan81@gmail.com>
2024-08-08 15:10:54 -0500

    test 2

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-let Person;
+let Person = require('./models/person');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
